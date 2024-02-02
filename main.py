a33895f3d4020a579696b0c03da68d0241e15f58
Theo Chromebook <theofarrell18@gmail.com>
2024-02-02 12:51:45 +0000

    setup done!

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import sys
+import os
 
+import cv2
+import numpy as np
+
+# path_to_images = '/home/theofarrell18/Assignments/Image-Processing/image_processing_files/xray_images'
+path_to_images = sys.argv[1] # path given as command line argument
+# check if valid path? like in ai search
+for images in os.listdir(path_to_images):
+    print(images)
