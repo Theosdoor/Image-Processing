d4cc35f972c83c45b2ad0f7f6e5f8facf56b3e4c
Theosdoor <theodore.farrell@durham.ac.uk>
2024-02-19 16:50:58 +0000

    trying to fix perspective

--- test.py
+++ test.py
@@ -24,9 +24,7 @@ def show_circles(image):
     else:
         exit("No circles found in image.")
 
-    cv2.imshow('detected circles', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imwrite('detected circles', image)
 
     return image
 
@@ -51,20 +49,105 @@ def match_circles(image):
 
 
     # cv2.imshow('black circle', black_circle)
-    cv2.imshow('matched circles', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('matched circles', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return image
 
 def process_image(image):
-    # cv2.imshow('image', image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    rows, cols, ch = image.shape
+    # # define size of circle
+    # width = image.shape[1]
+    # height = image.shape[0]
+    # radius = width // 2
 
-    image = match_circles(image)
+    # # create black circle mask
+    # black_circle = np.ones((height, width, 3), np.uint8)
+    # cv2.circle(black_circle, (int(width / 2), int(height / 2)),
+    #            radius, (0, 0, 0), -1)
 
-    # # hisogram equalisation (copilot)
-    # # convert to hsv colour space
+    # pad image with border
+    border_size = 6
+    image = cv2.copyMakeBorder(image, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+    mask = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # isolate black circle by thresholding
+    mask = cv2.inRange(mask, 0, 10)
+
+    # sharpen mask edges using laplacian
+    mask = cv2.Laplacian(mask, cv2.CV_8U)
+
+    # get 4 corners of black square
+    corners = cv2.goodFeaturesToTrack(mask, 4, 0.01, 10)
+    corners = corners.reshape(4, 2)
+
+    # get centers
+    print(corners)
+    # coord = (xmin, ymin, xmax, ymax)
+    coord = (corners[0][1], corners[1][1], corners[2][0], corners[3][1])
+    centerCoord = (coord[0]+(coord[2]/2), coord[1]+(coord[3]/2))
+    print(centerCoord)
+    print(mask.shape[0] / 2, mask.shape[1] / 2)
+
+    xshift = mask.shape[0] / 2 - centerCoord[0]
+    yshift = mask.shape[1] / 2 - centerCoord[1]
+
+    shift = np.full(image.shape, [xshift, yshift, 0])
+
+    # add shift to every element in image matrix
+    image = np.add(image, shift)
+
+    # put red circles on object corners
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    corners = corners.astype(np.int32)  # convert to integers
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(image, (x, y), 3, (0, 0, 255), -1)
+
+    # put circle in middle of image
+    cv2.circle(image, (int(image.shape[1] / 2), int(image.shape[0] / 2)), 3, (0, 0, 255), -1)
+
+    # shift perspective
+    pts1 = np.float32(corners)  # 4 corners of black square
+    pts2 = np.float32([[0, 0], [cols, 0], [0, rows], [cols, rows]])  # 4 corners of new image
+
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+
+    image = cv2.warpPerspective(image, M, (cols, rows))
+
+    
+
+
+    # # apply fft to mask
+    # rows, cols = mask.shape
+    # crow, ccol = rows // 2, cols // 2 # center of mask
+
+    # fft = np.fft.fft2(mask)
+    # fft_shifted = np.fft.fftshift(fft)
+
+    # # correct orientation using magnitude spectrum
+    # magnitude_spectrum = 20 * np.log(np.abs(fft_shifted))
+    # magnitude_spectrum = cv2.inRange(magnitude_spectrum, 0, 10)
+
+
+    # # apply low pass filtering
+    # # radius = 30
+    # # fft_shifted[crow-radius : crow+radius, ccol-radius : ccol+radius] = 0 # low pass filter
+
+    # # shift back
+    # fft_inv_shift = np.fft.ifftshift(fft_shifted)
+    # mask = np.fft.ifft2(fft_inv_shift)
+    # mask = np.abs(mask) # get magnitude
+
+
+    # image inpainting
+    # image = cv2.inpaint(image, mask, inpaintRadius=6, flags=cv2.INPAINT_NS)
+
+    # image = match_circles(image)
+
+    # hisogram equalisation (copilot)
+    # convert to hsv colour space
     # image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # # apply histogram equalisation to the V channel
@@ -73,6 +156,9 @@ def process_image(image):
 
     # # convert back to BGR
     # image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
+    # nlm filtering
+    # image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
 
     # # make brighter (copilot)
     # gain = 1.2
@@ -96,11 +182,11 @@ for tag in os.listdir(path_to_images):
     img_loaded = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
     # FOR ALL IMAGES
-    processed = process_image(img_loaded)
+    # processed = process_image(img_loaded)
     # cv2.imwrite(os.path.join('Results', tag), processed)
 
     # FOR TEST IMAGES
-    # images[tag] = img_loaded
+    images[tag] = img_loaded
 
 # ==============================================================================
 # TESTING (DELETE LATER)
@@ -110,7 +196,7 @@ for tag in os.listdir(path_to_images):
 healthy = 'im003-healthy.jpg'
 pneumonia = 'im053-pneumonia.jpg'
 
-img_name = pneumonia
+img_name = healthy
 
 # load image
 image = images[img_name]
